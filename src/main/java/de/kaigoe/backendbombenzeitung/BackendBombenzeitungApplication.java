1dd3fef38e5980500b669049e8c8fb0bc49bfa3b
Kai Götte <kai.goe@outlook.de>
2024-01-31 20:11:02 +0100

    Initial commit

---
+++ src/main/java/de/kaigoe/backendbombenzeitung/BackendBombenzeitungApplication.java
@@ -0,0 +1,13 @@
+package de.kaigoe.backendbombenzeitung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBombenzeitungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendBombenzeitungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/kaigoe/backendbombenzeitung/BackendBombenzeitungApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kaigoe.backendbombenzeitung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBombenzeitungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
